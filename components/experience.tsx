4b2f3f65ddd689c9543a7095a06ffaf3d915689d
Wei <71568978+weigriffiths@users.noreply.github.com>
2023-08-24 08:08:50 +1000

    update dark theme

--- components/experience.tsx
+++ components/experience.tsx
@@ -40,7 +40,7 @@ export default function Experience() {
               icon={item.icon}
               iconStyle={{
                 background:
-                  theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+                  theme === "light" ? "white" : "rgba(19, 24, 38)",
                 fontSize: "1.5rem",
               }}
             >
